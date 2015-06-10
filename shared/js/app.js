c4fa00e71264d4f35ea4ce2ef7156911ed0c6038
Andrew McConville <andrew@andrewmcconville.com>
2015-06-09 22:32:21 -0500

    toggles detail pane and list pane for mobile

--- shared/js/app.js
+++ shared/js/app.js
@@ -11,7 +11,7 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
 		.state('details', {
 			url: '/:url',			
 			templateUrl: 'components/details.html',
-			controller: function($scope, $state, $stateParams, Works){
+			controller: function($scope, $timeout, $state, $stateParams, Works){
 				var currentWork = Works.getWork($stateParams.url);
 				$scope.previousWorkURL = Works.getPreviousURL(currentWork.index);
 				$scope.nextWorkURL = Works.getNextURL(currentWork.index);
@@ -24,14 +24,28 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
 						$('#js-bricks').masonry({
 							itemSelector: '.masonry-brick'
 						});
-					}, 0);
+					}, 160);
 				};
 			}
 		});
 }]);
 
-app.controller('appCtrl', ['$scope', '$stateParams', '$timeout', 'Works', function($scope, $stateParams, $timeout, Works) {
+app.controller('appCtrl', ['$scope', '$state', '$stateParams', '$timeout', 'Works', function($scope, $state, $stateParams, $timeout, Works) {
 	$scope.works = Works.getWorks();
+	
+	$timeout(function(){
+		$scope.updateState();
+	}, 0);
 
-	$scope.showDetails = false;
+	$scope.updateState = function(){
+		$(window).scrollTop(0);
+
+		if($state.current.name == 'details') {
+			$scope.detailsActive = true;
+			return $scope.detailsActive;
+		} else {
+			$scope.detailsActive = false;
+			return $scope.detailsActive;
+		}		
+	};
 }]);
